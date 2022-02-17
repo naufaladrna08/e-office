86f2e99abfc28fb4a98552985ca8fdfe4411f94e
naufaladrna08 <naufaladrn90@gmail.com>
2022-02-17 16:05:06 +0700

    Added backend
    Using Laravel 9

---
+++ e-office-backend/app/Classes/Response.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Classes;
+
+class Response {
+  public static function pretty($code = 200, $status, $message, $data = null) {
+    $data = [
+      'code' => $code,
+      'status' => $status,
+      'message' => $message,
+      'data' => $data
+    ];
+
+    return response()->json($data);
+  }
+};
