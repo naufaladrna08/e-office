d23b3180d1fa2a2b3cf011cf469bf28297658855
naufaladrna08 <naufaladrn90@gmail.com>
2022-02-20 16:29:27 +0700

    update routes

--- e-office-backend/routes/api.php
+++ e-office-backend/routes/api.php
@@ -14,9 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/register', [App\Http\Controllers\API\AuthController::class, 'register']);
-Route::post('/login', [App\Http\Controllers\API\AuthController::class, 'login']);
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
   return auth()->user();
 });
+
+/* Mail CRUD */
+Route::middleware('auth:sanctum')->post('/mail/create', [App\Http\Controllers\MailController::class, 'create']);
+Route::middleware('auth:sanctum')->get('/mail/read', [App\Http\Controllers\MailController::class, 'read']);
+Route::middleware('auth:sanctum')->get('/mail/read-all', [App\Http\Controllers\MailController::class, 'readAll']);
+Route::middleware('auth:sanctum')->post('/mail/update', [App\Http\Controllers\MailController::class, 'update']);
+Route::middleware('auth:sanctum')->post('/mail/delete', [App\Http\Controllers\MailController::class, 'delete']);
+
+Route::post('/register', [App\Http\Controllers\API\AuthController::class, 'register']);
+Route::post('/login', [App\Http\Controllers\API\AuthController::class, 'login']);
+Route::post('/logout', [App\Http\Controllers\API\AuthController::class, 'logout']);
