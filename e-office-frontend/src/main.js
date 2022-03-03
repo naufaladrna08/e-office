5ba561ca1d19ec22557499efd3219f52c7e96345
naufaladrna08 <naufaladrn90@gmail.com>
2022-03-03 20:47:13 +0700

    added font awesome

--- e-office-frontend/src/main.js
+++ e-office-frontend/src/main.js
@@ -5,6 +5,8 @@ import store from './store'
 
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import '@/assets/style.scss'
 
 Vue.config.productionTip = false
 
