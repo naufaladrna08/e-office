191928c65213009879cd5efb93a527d93b8bb7fe
naufaladrna08 <naufaladrn90@gmail.com>
2022-03-03 20:48:25 +0700

    temporary remove root route

--- e-office-frontend/src/router/index.js
+++ e-office-frontend/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import DashboardView from '../views/DashboardView.vue'
@@ -9,11 +9,11 @@ import DashboardView from '../views/DashboardView.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: HomeView
-  },
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: HomeView
+  // },
   {
     path: '/login',
     name: 'Login',
