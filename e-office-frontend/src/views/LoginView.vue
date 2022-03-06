5ab3ee812145a85384c98fb798c4d83441a39ec1
naufaladrna08 <naufaladrn90@gmail.com>
2022-03-06 17:21:13 +0700

    update view

--- e-office-frontend/src/views/LoginView.vue
+++ e-office-frontend/src/views/LoginView.vue
@@ -1,37 +1,96 @@
 <template>
-  <section class="container my-4">
-    <div class="row">
-      <div class="col-md-4">
-        <div class="card">
-          <div class="card-body">
-            <div class="my-4">
-              <h3> Selamat Datang </h3>
-              <p class="lead">
-                E-OFFICE DPC SPPI II PTP
-              </p>
+  <div id="root-page">
+    <img v-bind:src="images.top_right" id="top-right">
+    <img v-bind:src="images.middle_left" id="middle-left">
+    <img v-bind:src="images.computer" id="computer">
+
+    <section class="container my-4">
+      <nav class="navbar navbar-light">
+        <a class="navbar-brand" href="#">
+          <img v-bind:src="images.logo">
+        </a>
+      </nav>
+
+      <div class="row">
+        <div class="col-md-4">
+          <div class="card">
+            <div class="card-body">
+              <div class="my-4">
+                <h3> Selamat Datang </h3>
+                <p class="lead">
+                  E-OFFICE DPC SPPI II PTP
+                </p>
+              </div>
+              <form action="javascript:void(0)" class="row" method="post">
+                <div class="input-group mb-3">
+                  <span class="input-group-text" id="basic-addon1"><i class="fa fa-user" aria-hidden="true"></i></span>
+                  <input type="text" v-model="auth.username" name="username" id="username" class="form-control" aria-label="Username" aria-describedby="basic-addon1" placeholder="Username">
+                </div>
+                <div class="input-group mb-3">
+                  <span class="input-group-text" id="basic-addon1"><i class="fa fa-solid fa-lock"></i></span>
+                  <input type="text" v-model="auth.password" name="password" id="password" class="form-control" aria-label="password" aria-describedby="basic-addon1" placeholder="***">
+                </div>
+                <div class="col-12 mt-4">
+                  <button type="submit" :disabled="processing" @click="login" class="btn btn-primary btn-block">
+                    {{ processing ? "Please wait" : "Login" }}
+                  </button>
+                </div>
+              </form>
             </div>
-            <form action="javascript:void(0)" class="row" method="post">
-              <div class="input-group mb-3">
-                <span class="input-group-text" id="basic-addon1"><i class="fa fa-user" aria-hidden="true"></i></span>
-                <input type="text" v-model="auth.username" name="username" id="username" class="form-control" aria-label="Username" aria-describedby="basic-addon1" placeholder="Username">
-              </div>
-              <div class="input-group mb-3">
-                <span class="input-group-text" id="basic-addon1"><i class="fa fa-solid fa-lock"></i></span>
-                <input type="text" v-model="auth.password" name="password" id="password" class="form-control" aria-label="password" aria-describedby="basic-addon1" placeholder="***">
-              </div>
-              <div class="col-12 mt-4">
-                <button type="submit" :disabled="processing" @click="login" class="btn btn-primary btn-block">
-                  {{ processing ? "Please wait" : "Login" }}
-                </button>
-              </div>
-            </form>
           </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </div>
 </template>
 
+<style scoped>
+  #root-page {
+    background-image: url('~@/assets/background.svg');
+    height: 100vh;
+    margin-top: -2em;
+  }
+
+  #top-right {
+    position: absolute;
+    right: 0;
+    width: 40%;
+    overflow: hidden;
+    line-height: 0;
+  }
+
+  #middle-left {
+    position: absolute;
+    top: 24%;
+    left: 0;
+    width: 10%;
+    overflow: hidden;
+    line-height: 0;
+  }
+
+  #computer {
+    position: absolute;
+    top: 15%;
+    right: 3%;
+    width: 40%;
+    overflow: hidden;
+    line-height: 0;
+  }
+
+  .navbar {
+    padding: 4em 0;
+  }
+
+  .card {
+    border-radius: 2rem;
+    background: #F4F0F0;
+  }
+
+  .btn-primary {
+    background-color: #02BFFF;
+    border: 1px solid #02BFFF;
+  }
+</style>
 
 <script>
 import axios from 'axios'
@@ -45,7 +104,13 @@ export default {
         username: '',
         password: ''
       },
-      processing: false
+      processing: false,
+      images: {
+        "top_right": require("../assets/particles/top-right-login.svg"),
+        "middle_left": require("../assets/particles/middle-left-login.svg"),
+        "computer": require("../assets/particles/computer-login.svg"),
+        "logo": require("../assets/logo.svg")
+      }
     }
   },
   methods: {
