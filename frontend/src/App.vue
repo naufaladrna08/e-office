e0c4565f0c8a157b4f532cc76bb7fa9380447b15
naufaladrna08 <naufaladrn90@gmail.com>
2022-04-11 22:00:07 +0700

    move navbar to app

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,67 @@
 <template>
-  <router-view />
+  <div>
+    <div v-if="user != null">
+      <div class="sidebar d-flex flex-column flex-shrink-0 bg-light" style="width: 4.5rem;">
+        <a href="/" class="d-block p-3 link-dark text-decoration-none" title="Icon-only" data-bs-toggle="tooltip" data-bs-placement="right">
+          <svg class="bi" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
+          <span class="visually-hidden">Icon-only</span>
+        </a>
+        <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
+          <li class="nav-item">
+            <a href="#" class="nav-link active py-3 border-bottom" aria-current="page" title="Home" data-bs-toggle="tooltip" data-bs-placement="right">
+              <i class="fa fa-home" style="width: 24px; height: 24px"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#" class="nav-link py-3 border-bottom" title="Dashboard" data-bs-toggle="tooltip" data-bs-placement="right">
+              <i class="fa fa-user" style="width: 24px; height: 24px"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#" class="nav-link py-3 border-bottom" title="Orders" data-bs-toggle="tooltip" data-bs-placement="right">
+              <i class="fa fa-inbox" style="width: 24px; height: 24px"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#" class="nav-link py-3 border-bottom" title="Orders" data-bs-toggle="tooltip" data-bs-placement="right">
+              <i class="fa fa-archive" style="width: 24px; height: 24px"></i>
+            </a>
+          </li>
+          <li>
+            <a href="#" class="nav-link py-3 border-bottom" title="Orders" data-bs-toggle="tooltip" data-bs-placement="right">
+              <i class="fa fa-sign-out" style="width: 24px; height: 24px" @click="logout"></i>
+            </a>
+          </li>
+        </ul>
+        <div class="dropdown border-top">
+          <a href="#" class="d-flex align-items-center justify-content-center p-3 link-dark text-decoration-none dropdown-toggle" id="dropdownUser3" data-bs-toggle="dropdown" aria-expanded="false">
+            <img src="https://github.com/mdo.png" alt="mdo" width="24" height="24" class="rounded-circle">
+          </a>
+          <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser3">
+            <li><a class="dropdown-item" href="#">New project...</a></li>
+            <li><a class="dropdown-item" href="#">Settings</a></li>
+            <li><a class="dropdown-item" href="#">Profile</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="javascript:void(0);" @click="logout">Sign out</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <router-view />
+  </div>
 </template>
 
 <script>
+import store from './store'
+
 export default {
   name: 'App',
   components: {
+  },
+  data() {
+    return {
+      user: store.state.auth.user,
+    }
   }
 }
 </script>
@@ -17,4 +73,12 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #222222;
 }
+
+.sidebar {
+  background-color: rgba(115, 24, 152, 1);
+  position: fixed;
+  height: 100vh !important;
+  z-index: 9999 !important;
+}
+
 </style>
--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -1,52 +1,6 @@
 <template>
   <div class="root">
     <div class="main">
-      <div class="sidebar d-flex flex-column flex-shrink-0 bg-light" style="width: 4.5rem;">
-        <a href="/" class="d-block p-3 link-dark text-decoration-none" title="Icon-only" data-bs-toggle="tooltip" data-bs-placement="right">
-          <svg class="bi" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-          <span class="visually-hidden">Icon-only</span>
-        </a>
-        <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
-          <li class="nav-item">
-            <a href="#" class="nav-link active py-3 border-bottom" aria-current="page" title="Home" data-bs-toggle="tooltip" data-bs-placement="right">
-              <i class="fa fa-home" style="width: 24px; height: 24px"></i>
-            </a>
-          </li>
-          <li>
-            <a href="#" class="nav-link py-3 border-bottom" title="Dashboard" data-bs-toggle="tooltip" data-bs-placement="right">
-              <i class="fa fa-user" style="width: 24px; height: 24px"></i>
-            </a>
-          </li>
-          <li>
-            <a href="#" class="nav-link py-3 border-bottom" title="Orders" data-bs-toggle="tooltip" data-bs-placement="right">
-              <i class="fa fa-inbox" style="width: 24px; height: 24px"></i>
-            </a>
-          </li>
-          <li>
-            <a href="#" class="nav-link py-3 border-bottom" title="Orders" data-bs-toggle="tooltip" data-bs-placement="right">
-              <i class="fa fa-archive" style="width: 24px; height: 24px"></i>
-            </a>
-          </li>
-          <li>
-            <a href="#" class="nav-link py-3 border-bottom" title="Orders" data-bs-toggle="tooltip" data-bs-placement="right">
-              <i class="fa fa-sign-out" style="width: 24px; height: 24px" @click="logout"></i>
-            </a>
-          </li>
-        </ul>
-        <div class="dropdown border-top">
-          <a href="#" class="d-flex align-items-center justify-content-center p-3 link-dark text-decoration-none dropdown-toggle" id="dropdownUser3" data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="https://github.com/mdo.png" alt="mdo" width="24" height="24" class="rounded-circle">
-          </a>
-          <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser3">
-            <li><a class="dropdown-item" href="#">New project...</a></li>
-            <li><a class="dropdown-item" href="#">Settings</a></li>
-            <li><a class="dropdown-item" href="#">Profile</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="javascript:void(0);" @click="logout">Sign out</a></li>
-          </ul>
-        </div>
-      </div>
-
       <div class="dashboard">
         <nav class="navbar navbar-light bg-light fixed-top">
           <div class="container px-2">
@@ -140,9 +94,10 @@
           <div class="media bg-lightblue mt-4" id="mail">
             <div class="media-body p-2">
               <DataTables 
-                tclass="table table-hover w-100 p-2" 
+                tclass="table table-hover table-bordered w-100 p-2" 
                 url="mail/read-all"
                 :columns="arr"
+                useNumber="true"
                 id="overview-table"
               />
             </div>
