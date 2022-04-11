0a8fdee579c711576003e003e525cc90ec35ec83
naufaladrna08 <naufaladrn90@gmail.com>
2022-04-11 22:06:48 +0700

    update route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import DashboardView from '../views/DashboardView.vue'
+import InboxView from '../views/InboxView.vue'
 import store from '../store'
 
 const routes = [
