d7e4ce172274839ba5e52776fbb75351886f23fb
naufaladrna08 <naufaladrn90@gmail.com>
2022-04-13 08:33:43 +0700

    change variable name

--- frontend/src/components/DataTables.vue
+++ frontend/src/components/DataTables.vue
@@ -48,7 +48,7 @@
     <table v-bind:class="tclass">
       <thead>
         <tr>
-          <th v-for="column in columns" :key="column.name" @click="sortByColumn(column)"> 
+          <th v-for="column in columnList" :key="column" @click="sortByColumn(column)"> 
             {{ column.toUpperCase() }} 
             
             <span v-if="column === sortField">
@@ -65,7 +65,7 @@
           </div>
         </tr>
         <tr v-for="data in tableData" :key="data.data">
-          <td v-for="col in data" :key="col.name"> {{ col }} </td>
+          <td v-for="col in data" :key="col"> {{ col }} </td>
         </tr>
       </tbody>
     </table>
@@ -91,7 +91,8 @@ export default {
   props: {
     columns: { type: Array, required: true },
     tclass: { type: Number, required: true },
-    url: { type: String, required: true }
+    url: { type: String, required: true },
+    useNumber: { type: Boolean, required: false }
   },
   data() {
     return {
@@ -103,12 +104,20 @@ export default {
       perPage: 5,
       pagination: { to: 1, from: 1  },
       page: 1,
-      total: 1
+      total: 1,
+      columnList: this.columns,
+      number: this.useNumber
     }
   },
   created() {
+    // if (this.number) {
+    //   for (let i = 0; i < this.pagination.total; i++) {
+    //     this.columnList[i].unshift({no: i})
+    //   }
+    // }
+
     this.fetchData()
-  },
+},
   methods: {
     async fetchData() {
       try {
@@ -121,8 +130,18 @@ export default {
         }
 
         const {data} = await axios.get(this.url, {params})
-        this.tableData = data.data
+        this.tableData = data.data 
         this.pagination = data.meta
+
+        if (this.number) {
+          this.columnList.unshift('no')
+          console.log(this.columnList)
+          
+          data.data.forEach((i) => {
+            console.log(i)
+          })
+        }
+        
       } catch (e) {
         console.log(e)
       }
@@ -158,6 +177,12 @@ export default {
 thead tr th {
   cursor: pointer;
 }
+
+tr {
+  min-height: 60px;
+  vertical-align: middle; 
+}
+
 
 .btn {
   border-radius: 0 0.25rem 0.25rem 0;
