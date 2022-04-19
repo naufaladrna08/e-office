909b97458e223af06857f8d880e8c4bf254ae080
naufaladrna08 <naufaladrn90@gmail.com>
2022-04-19 12:21:27 +0700

    fixed navbar active color

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,29 +4,29 @@
       <div class="sidebar d-flex flex-column flex-shrink-0 bg-light" style="width: 4.5rem;">
         <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
           <li class="nav-item">
-            <a href="#" class="nav-link active py-3 border-bottom" aria-current="page" title="Home" data-bs-toggle="tooltip" data-bs-placement="right">
+            <router-link to="/dashboard" class="nav-link py-3 border-bottom" :class="currentRoute === 'Dashboard' ? 'active' : ''" title="Dashboard">
               <i class="fa fa-home" style="width: 24px; height: 24px"></i>
-            </a>
+            </router-link>
           </li>
           <li>
-            <a href="#" class="nav-link py-3 border-bottom" title="Dashboard" data-bs-toggle="tooltip" data-bs-placement="right">
+            <router-link to="/user" class="nav-link py-3 border-bottom" :class="currentRoute === 'User' ? 'active' : ''" title="User">
               <i class="fa fa-user" style="width: 24px; height: 24px"></i>
-            </a>
+            </router-link>
           </li>
           <li>
-            <router-link to="/inbox" class="nav-link py-3 border-bottom" title="Inbox" data-bs-toggle="tooltip" data-bs-placement="right">
+            <router-link to="/inbox" class="nav-link py-3 border-bottom" :class="currentRoute === 'Inbox' ? 'active' : ''" title="Inbox">
               <i class="fa fa-inbox" style="width: 24px; height: 24px"></i>
             </router-link>
           </li>
           <li>
-            <a href="#" class="nav-link py-3 border-bottom" title="Orders" data-bs-toggle="tooltip" data-bs-placement="right">
+            <router-link to="/archive" class="nav-link py-3 border-bottom" :class="currentRoute === 'Archive' ? 'active' : ''" title="Archive">
               <i class="fa fa-archive" style="width: 24px; height: 24px"></i>
-            </a>
+            </router-link>
           </li>
           <li>
-            <a href="#" class="nav-link py-3 border-bottom" title="Orders" data-bs-toggle="tooltip" data-bs-placement="right">
+            <router-link to="/logout" class="nav-link py-3 border-bottom" title="Logout">
               <i class="fa fa-sign-out" style="width: 24px; height: 24px" @click="logout"></i>
-            </a>
+            </router-link>
           </li>
         </ul>
         <div class="dropdown border-top">
@@ -58,6 +58,7 @@ export default {
   data() {
     return {
       user: store.state.auth.authenticated,
+      currentRoute: 'dashboard'
     }
   },
   methods: {
@@ -72,8 +73,9 @@ export default {
   watch:{
     $route() {
       this.user = store.state.auth.authenticated
+      this.currentRoute = this.$route.name
     }
-  } 
+  }
 }
 </script>
 
