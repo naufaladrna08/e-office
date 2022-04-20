9c655d9281d4c555e131d8335c6b0bcba38a4971
naufaladrna08 <naufaladrn90@gmail.com>
2022-04-20 23:36:29 +0700

    using json for response

--- frontend/src/store/subscriber.js
+++ frontend/src/store/subscriber.js
@@ -5,13 +5,15 @@ store.subscribe((mutation) => {
   if (mutation.type == 'auth/SET_TOKEN') {
     if (mutation.payload) {
       axios.defaults.headers = {
-        'Authorization': `Bearer ${mutation.payload}`
+        'Authorization': `Bearer ${mutation.payload}`,
+        'Accept': 'application/json'
       }
 
       localStorage.setItem('token', mutation.payload)
     } else {
       axios.defaults.headers = {
-        'Authorization': null
+        'Authorization': null,
+        'Accept': 'application/json'
       }
 
       localStorage.setItem('token', null)
