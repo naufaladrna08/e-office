f370bfdfb4576700f40ea0747538aef49b9d8c63
naufaladrna08 <naufaladrn90@gmail.com>
2022-05-04 21:31:59 +0700

    added form template

--- frontend/src/views/InboxView.vue
+++ frontend/src/views/InboxView.vue
@@ -1,10 +1,135 @@
 <template>
   <div>
-    <div class="container text-center">
+    <div class="container">
       <form @submit.prevent="send">
-        <b class="my-4"> BUAT SURAT </b>
-
         <div class="row">
+          <div class="col-md-6">
+            <p class="lead"> * Kepala Surat </p>
+
+            <div class="form-group my-2">
+              <label class="fw-bolder mb-2"> Jenis / No / Tanggal </label>
+              
+              <div class="row mt-2">
+                <div class="col">
+                  <input type="text" placeholder="AAA" class="form-control">
+                </div>
+                <div class="col">
+                  <input type="text" placeholder="AAA" class="form-control">
+                </div>
+                <div class="col">
+                  <input type="text" placeholder="AAA" class="form-control">
+                </div>
+              </div>
+            </div>
+            <div class="form-group my-2">
+              <label class="fw-bolder mb-2"> Perihal </label>
+              <input type="text" class="form-control">
+            </div>
+            <div class="form-group my-2">
+              <label class="fw-bolder mb-2"> Klasifikasi Masalah </label>
+              <input type="text" class="form-control">
+            </div>
+            <div class="form-group my-2">
+              <label class="fw-bolder mb-2"> Lampiran </label>
+              
+              <div class="row mt-2">
+                <div class="col">
+                  <input type="text" placeholder="AAA" class="form-control">
+                </div>
+                <div class="col">
+                  <input type="text" placeholder="AAA" class="form-control">
+                </div>
+                <div class="col">
+                  <input type="text" placeholder="AAA" class="form-control">
+                </div>
+              </div>
+            </div>
+
+            <p class="lead mt-4"> * Pengirim </p>
+
+            <div class="form-group my-2">
+              <label class="fw-bolder mb-2"> Jabatan </label>
+              <input type="text" value="Autocomplete" class="form-control" disabled>
+            </div>
+            <div class="form-group my-2">
+              <label class="fw-bolder mb-2"> NIPP / Nama </label>
+
+              <div class="row mt-2">
+                <div class="col">
+                  <input type="text" value="Autocomplete" class="form-control" disabled>
+                </div>
+                <div class="col">
+                  <input type="text" value="Autocomplete" class="form-control" disabled>
+                </div>
+              </div>
+            </div>
+            <div class="form-group my-2">
+              <label class="fw-bolder mb-2"> Divisi </label>
+              <input type="text" value="Autocomplete" class="form-control" disabled>
+            </div>
+            <div class="form-group my-2">
+              <label class="fw-bolder mb-2"> Cab / Dir </label>
+              <input type="text" value="Autocomplete" class="form-control" disabled>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <p class="lead"> * Penerima </p>
+
+            <div class="form-group my-2">
+              <label class="fw-bolder mb-2"> Kepada (Jabatan) </label>
+              
+              <div class="input-group">
+                <input type="text" class="form-control">
+                <div class="btn btn-primary"> + </div>
+              </div>
+            </div>
+            <div class="form-group my-2">
+              <label class="fw-bolder mb-2"> Tembusan (Jabatan) </label>
+              
+              <div class="input-group">
+                <input type="text" class="form-control">
+                <div class="btn btn-primary"> + </div>
+              </div>
+            </div>
+            <div class="form-group my-2">
+              <label class="fw-bolder mb-2"> Kepada (Pekerja) </label>
+              
+              <div class="input-group">
+                <input type="text" class="form-control">
+                <div class="btn btn-primary"> + </div>
+              </div>
+            </div>
+            <div class="form-group my-2">
+              <label class="fw-bolder mb-2"> Tembusan (Pekerja) </label>
+              
+              <div class="input-group">
+                <input type="text" class="form-control">
+                <div class="btn btn-primary"> + </div>
+              </div>
+            </div>
+            <div class="form-group my-2">
+              <label class="fw-bolder mb-2"> Tembusan (Non Pekerja) </label>
+              
+              <div class="input-group">
+                <input type="text" class="form-control">
+                <div class="btn btn-primary"> + </div>
+              </div>
+            </div>
+
+            <p class="lead"> * Pemeriksa </p>
+
+            <div class="form-group my-2">
+              <label class="fw-bolder mb-2"> Pemeriksa </label>
+              
+              <div class="input-group">
+                <input type="text" class="form-control">
+                <div class="btn btn-primary"> + </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="row mt-4">
           <div class="col-md-12">
             <div class="form-group my-2">
               <input
