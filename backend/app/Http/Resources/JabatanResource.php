5ca1612210ee889cc3051e5c1d88f8a3f0730fe7
naufaladrna08 <naufaladrn90@gmail.com>
2022-05-05 21:01:33 +0700

    update

--- backend/app/Http/Resources/JabatanResource.php
+++ backend/app/Http/Resources/JabatanResource.php
@@ -4,16 +4,17 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class JabatanResource extends JsonResource
-{
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
-    public function toArray($request)
-    {
-        return parent::toArray($request);
-    }
+class JabatanResource extends JsonResource {
+  /**
+   * Transform the resource into an array.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+   */
+  public function toArray($request) {
+    return [
+      'code_divisi' => $this->code_divisi,
+      'nama_divisi' => $this->nama_divisi
+    ];
+  }
 }
