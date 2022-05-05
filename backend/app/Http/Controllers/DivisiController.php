81e4f6f61b291dd6d4cdc730325107232a581818
naufaladrna08 <naufaladrn90@gmail.com>
2022-05-05 21:41:09 +0700

    implement crud

--- backend/app/Http/Controllers/DivisiController.php
+++ backend/app/Http/Controllers/DivisiController.php
@@ -4,17 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Validator;
-use App\Models\Divisi;
+use App\Models\ParamDivisi;
+use App\Http\Resources\DivisiResource;
+use App\Classes\Response;
+use Illuminate\Support\Facades\DB;
 
 class DivisiController extends Controller {
   public function create(Request $r) {
     $validator = Validator::make($r->all(), [
-      'code_divisi' => 'required|string|max:8|min:8',
-      'nama_divisi' => 'required|string|max:8|min:8'
+      'code_divisi' => 'required|string|max:8|min:4',
+      'nama_divisi' => 'required|string|max:255|min:4'
     ]);
 
     if (!$validator->fails()) {
-      $data = Divisi::create([
+      $data = ParamDivisi::create([
         'code_divisi' => $r->code_divisi,
         'nama_divisi' => $r->nama_divisi
       ]);
@@ -26,22 +29,54 @@ class DivisiController extends Controller {
       return Response::pretty(500, 'Failed', 'Internal Server Error', null);
     }
 
-    return Response::pretty(200, 'Success', 'Data has been created', [
-      'user' => $user,
-      'access_token' => $token,
-      'token_type' => 'Bearer'
-    ]);
+    return Response::pretty(200, 'Success', 'Data has been created', $data);
   }
 
   public function read(Request $r) {
-    
+    $fields = ['code_divisi', 'nama_divisi'];
+    $oderField = ['asc', 'desc'];
+
+    if ($r->order == null || $r->field != null ||
+       !in_array($r->order, $oderField) || !in_array($r->field, $fields)) {
+      $r->order = 'asc';
+      $r->field = 'code_divisi';
+    }
+
+    $query = DB::table('param_divisi')
+      ->select('code_divisi', 'nama_divisi')
+      ->orderBy($r->field, $r->order);
+
+    if (!is_null($r->search)) {
+      $query = $query 
+        ->where('code_divisi', 'like', '%'.$r->search.'%')
+        ->orWhere('nama_divisi', 'like', '%'.$r->search.'%');
+    }
+
+    return DivisiResource::collection($query->paginate(10));
   }
 
   public function update(Request $r) {
+    $data = ParamDivisi::where('code_divisi', $r->old_code_divisi)->first();
+    $data->code_divisi = $r->code_divisi != null ? $r->code_divisi : $data->code_divisi;
+    $data->nama_divisi = $r->nama_divisi != null ? $r->nama_divisi : $data->nama_divisi;
 
+    if (!$data->update()) {
+      return Response::pretty(500, 'Failed', 'Internal Server Error', null);
+    }
+
+    return Response::pretty(200, 'Success', 'Data has been saved', $data);
   }
 
   public function delete(Request $r) {
-    
+    $data = [];
+    $divisi = ParamDivisi::where('code_divisi', $r->code_divisi)->delete();
+
+    if ($divisi) {
+      $data = Response::pretty(200, 'Success', 'Deleted', null);
+    } else {
+      $data = Response::pretty(500, 'Failed', 'Internal Server Error', null);
+    }
+
+    return $data;
   }
 }
