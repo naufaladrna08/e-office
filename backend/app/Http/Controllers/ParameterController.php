95a5d8f91461e860ab30d8a37cb4e6aa780371c8
naufaladrna08 <naufaladrn90@gmail.com>
2022-05-09 22:46:10 +0700

    update

--- backend/app/Http/Controllers/ParameterController.php
+++ backend/app/Http/Controllers/ParameterController.php
@@ -4,39 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Models\Parameter;
 use Illuminate\Http\Request;
+use App\Classes\Response;
 
 class ParameterController extends Controller {
-  /**
-   * Store a newly created resource in storage.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @return \Illuminate\Http\Response
-   */
-  public function store(Request $request) {
+  public function storeOrUpdate(Request $r) {
     $data = [];
     
-    $model = new Parameter;
-    $model->pname = $request->pname;
-    $model->pcode = $request->pcode;
-    $model->description = $request->description;
-
-    if ($model->save()) {
-      $data = Response::pretty(500, "Failed", "Internal Server Error", null);
-    } else {
-      $data = Response::pretty(200, "Success", "Data has been saved", $model);
-    }
-
-    return $data;    
-  }
-  public function update(Request $r) {
-    $data = [];
-    
-    $model = Paramter::firstOrCreate([
+    $model = Parameter::updateOrCreate([
       'pcode' => $r->pcode
     ], [
       'pname' => $r->pname,
       'description' => $r->description
     ]);
+
+    return Response::pretty(200, 'Success', 'Data has been saved', $model);
+  }
+  
+  public function fetch(Request $r) {
+    $data = [];
+    
+    $model = Parameter::where('pcode', $r->pcode)->firstOrFail();
 
     return Response::pretty(200, 'Success', 'Data has been saved', $model);
   }
