9d3036ff964b8fb40b374b8b052ee4f266374e18
naufaladrna08 <naufaladrn90@gmail.com>
2022-05-22 23:53:18 +0700

    change table name

--- backend/database/migrations/2022_05_09_151114_create_parameters_table.php
+++ backend/database/migrations/2022_05_09_151114_create_parameters_table.php
@@ -11,7 +11,7 @@ return new class extends Migration {
    * @return void
    */
   public function up() {
-    Schema::create('parameters', function (Blueprint $table) {
+    Schema::create('parameter', function (Blueprint $table) {
       $table->id();
       $table->string('pcode', 255);
       $table->string('pname', 255);
