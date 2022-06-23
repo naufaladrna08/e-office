f8e8846c661eee5c90ec1f219886bf389a8f36ad
naufaladrna08 <naufaladrn90@gmail.com>
2022-06-23 22:15:51 +0700

    added inbox view

---
+++ frontend/src/views/Mail/InboxView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <DataTables
+      url="mail/inbox"
+      tclass="table table-bordered w-100 p-2" 
+      :columns="inboxColumn"
+      useNumber="1"
+      useAssigner="0"
+      ref="inbox"
+    />
+  </div>  
+</template>
+
+<script>
+// import axios from 'axios'
+import DataTables from '../../components/DataTables.vue'
+
+export default {
+  name: 'InboxView',
+  components: {
+    DataTables
+  },
+  data() {
+    return {
+      inboxColumn: {
+        no_surat: 'No. Surat',
+        perihal: 'Perihal',
+        jenis_surat: 'Jenis',
+        status: 'Status',
+        tanggal: 'Tanggal'
+      },
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
