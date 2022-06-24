0fee1d660ed2ce4be0dcc4f57586be00fd8070bf
naufaladrna08 <naufaladrn90@gmail.com>
2022-06-24 23:26:19 +0700

    add simple mail reader

---
+++ frontend/src/views/Mail/ReadView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div id="body" v-html="content"> </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'ReadView',
+  data() {
+    return {
+      id: this.$route.params.id,
+      content: null
+    }
+  },
+  created() {
+    axios.get('/mail/read/' + this.id).then((resp) => {
+      this.content = resp.data.data.description
+    })
+  }
+}
+</script>
+
+<style>
+#body {
+  width: 100%;
+}
+</style>
