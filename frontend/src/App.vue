03044417c32d2985c62046ef4c6b94563cc37169
naufaladrna08 <naufaladrn90@gmail.com>
2022-07-06 23:36:10 +0700

    update header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -53,11 +53,8 @@
       </div>
 
       <nav class="navbar navbar-light bg-light">
-        <div class="container px-2">
-          <form class="d-flex">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
+        <div class="container px-2 my-2">
+          <b> E-Office </b>
         </div>
       </nav>
 
